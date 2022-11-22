322810669208608da8f50860ad4cc2361bbd811c
mosspaul <gmosspaul18@gmail.com>
2022-11-22 10:53:11 -0600

    Build Team constructor and playMatch function

--- sim.js
+++ sim.js
@@ -1,16 +1,43 @@
-function team(teamName, teamRating) {
-    this.teamName = teamName;
-    this.teamRating = teamRating;
-}
-
-
 function playMatch(team1, team2) {
     return team1.teamRating < team2.teamRating ? team1 : team2;
 }
 
-/*let group = {
-    team1: new team("Netherlands", 8),
-    team2: new team("Senegal", 18),
-    team3: new team("Ecuador", 44),
-    team4: new team("Quatar", 50),
-}*/
+
+function Team(teamName, teamRating) {
+    this.teamName = teamName;
+    this.teamRating = teamRating;
+}
+
+function Group(team1, team2, team3, team4) {
+    this.team1 = team1;
+    this.team2 = team2;
+    this.team3 = team3;
+    this.team4 = team4;
+    this.simMatches = () => 
+        [
+            playMatch(team1, team2),
+            playMatch(team1, team3),
+            playMatch(team1, team4),
+            playMatch(team2, team3),
+            playMatch(team2, team4),
+            playMatch(team3, team4)
+        ];  
+}
+
+/*
+* GROUP INITIALIZER
+*/
+let groupA = new Group(
+     new Team("Netherlands", 8),
+     new Team("Senegal", 18),
+     new Team("Ecuador", 44),
+     new Team("Quatar", 50)
+);
+
+let groupB = new Group(
+    new Team("England", 5),
+    new Team("United States", 16),
+    new Team("Wales", 19),
+    new Team("Iran", 20)
+);
+
